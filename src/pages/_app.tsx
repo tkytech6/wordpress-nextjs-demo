2f874dec9a44ae4eb8d4000081e6ab0086c4d534
tkytech6 <50701735+tkytech6@users.noreply.github.com>
2021-05-05 20:45:40 +0900

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,34 +6,9 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta content="width=device-width,initial-scale=1.0" name="viewport" />
-        <link
-          href={`${process.env.NEXT_PUBLIC_WP_BASE_URL}/wp-content/themes/xeory_base/base.css?ver=5.7.1`}
-          id="base-css-css"
-          media="all"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          href={`${process.env.NEXT_PUBLIC_WP_BASE_URL}/wp-content/themes/xeory_base/lib/fonts/font-awesome-4.5.0/css/font-awesome.min.css?ver=5.7.1`}
-          id="font-awesome-css"
-          media="all"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          href={`${process.env.NEXT_PUBLIC_WP_BASE_URL}/wp-includes/css/dist/block-library/style.min.css?ver=5.7.1`}
-          id="wp-block-library-css"
-          media="all"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          href={`${process.env.NEXT_PUBLIC_WP_BASE_URL}/wp-content/themes/xeory_base/style.css?ver=5.7.1`}
-          id="main-css-css"
-          media="all"
-          rel="stylesheet"
-          type="text/css"
-        />
+        <link href="/css/base.css" id="base-css-css" media="all" rel="stylesheet" type="text/css" />
+        <link href="/css/font-awesome.min.css" id="font-awesome-css" media="all" rel="stylesheet" type="text/css" />
+        <link href="/css/style.css" id="main-css-css" media="all" rel="stylesheet" type="text/css" />
       </Head>
 
       <Component {...pageProps} />
