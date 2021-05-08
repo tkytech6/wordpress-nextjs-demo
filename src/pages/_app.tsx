06767859f624928344f3f34fa485f9601b8e0fb7
tkytech6 <50701735+tkytech6@users.noreply.github.com>
2021-05-08 12:20:15 +0900

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta content="width=device-width,initial-scale=1.0" name="viewport" />
         <link href="/css/base.css" id="base-css-css" media="all" rel="stylesheet" type="text/css" />
-        {/* <link href="/css/font-awesome.min.css" id="font-awesome-css" media="all" rel="stylesheet" type="text/css" /> */}
+        <link href="/css/font-awesome.min.css" id="font-awesome-css" media="all" rel="stylesheet" type="text/css" />
         <link href="/css/style.css" id="main-css-css" media="all" rel="stylesheet" type="text/css" />
       </Head>
 
